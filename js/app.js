c2253303cf0c3340dd927262e9f760f3a23105ce
Dev <avrajs.dev@gmail.com>
2022-05-21 16:04:04 +0300

    Completed navbar and finished homepage

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const menuBtn = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', () => navLinks.classList.toggle('open'));
